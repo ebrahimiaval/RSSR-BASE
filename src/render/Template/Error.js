d791650f83087d22b858e543ea91bfd42b9288dd
ebrahimiaval <m.ebrahimiaval@gmail.com>
2019-11-03 12:50:36 +0330

    add IS_DEVELOPMENT & IS_PRODUCTION constant

--- src/render/Template/Error.js
+++ src/render/Template/Error.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Helmet} from "react-helmet";
+import {IS_DEVELOPMENT} from "../../setup/constant";
+
 
 function Error(props) {
-    const isDevEnv = process.env.NODE_ENV === 'development';
-
     return (
         <div className="p-5">
             <Helmet title="خطای پردازش"/>
             {
-                !isDevEnv ?
+                !IS_DEVELOPMENT ?
                     <div className="px-3 pb-3" dir="rtl">
                         <h2>خطای پردازش</h2>
                         <p>متاسفانه در طول پردازش خطایی رخ داده است، در صورت امکان به صفحه قبل بازگردید و با پشتیبانی تماس بگیرید.</p>
@@ -19,7 +19,7 @@ function Error(props) {
                 {props.error.message}
             </div>
             {
-                isDevEnv ?
+                IS_DEVELOPMENT ?
                     <pre className="px-3 text-left">{props.error.stack}</pre>
                     :
                     <script dangerouslySetInnerHTML={{__html: 'console.log(`' + JSON.stringify(props.error.stack) + '`)'}}/>
--- src/setup/constant.js
+++ src/setup/constant.js
@@ -4,6 +4,9 @@
 export const IS_BROWSER = typeof window !== 'undefined';
 export const IS_SERVER = !IS_BROWSER;
 
+export const IS_DEVELOPMENT =  process.env.NODE_ENV === 'development';
+export const IS_PRODUCTION =  process.env.NODE_ENV === 'production';
+
 
 
 
