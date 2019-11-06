0577b9da948e811b021ba7d162b970fd9f85d83b
ebrahimiaval <m.ebrahimiaval@gmail.com>
2019-11-06 15:44:53 +0330

    fix loading bug (pass down props)

--- src/Partial/Loading/Loading.js
+++ src/Partial/Loading/Loading.js
@@ -1,30 +1,40 @@
 import PropTypes from 'prop-types';
 import "./loadingEffect.scss";
 
-const Loading = theProps => {
-    let children = theProps.children;
-
-    // add loading class
-    if (theProps.isLoading || theProps.reloading) {
-        children = {...children};
-        const props = {...children.props};
-
-
-        if (props.className === undefined)
-            props.className = '';
-
-        props.className += theProps.isLoading ? " loading-animate" : "";
-        props.className += theProps.reloading ? " reload-animate" : "";
-
-        children.props = props;
+const Loading = (props) => {
+    const {isLoading, reloading, children} = props;
+    const copy = {
+        props: {...props},
+        children: {...children},
+        childrenProps: {...children.props}
     }
 
-    return children;
-};
+    // add loading class
+    if (isLoading || reloading) {
+        if (children.props.className === undefined)
+            copy.childrenProps.className = '';
+
+        if (isLoading) {
+            copy.childrenProps.className += " loading-animate";
+            delete copy.props.isLoading;
+        }
+
+        if (reloading) {
+            copy.childrenProps.className += " reload-animate";
+            delete copy.props.reloading;
+        }
+
+        copy.children.props = copy.childrenProps;
+    }
+
+    copy.children.props = {...copy.childrenProps, ...copy.props}
+
+    return copy.children;
+}
 
 Loading.propTypes = {
     isLoading: PropTypes.bool,
     reloading: PropTypes.bool,
-};
+}
 
 export default Loading;
