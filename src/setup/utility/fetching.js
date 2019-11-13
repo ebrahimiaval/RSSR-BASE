97ed852f31f48843ca48871e8fcb4fd419acc400
ebrahimiaval <m.ebrahimiaval@gmail.com>
2019-11-13 15:02:36 +0330

    fix fetching bug

--- src/setup/utility/fetching.js
+++ src/setup/utility/fetching.js
@@ -4,7 +4,13 @@ import axios from "axios";
 export const fetching = function (options, thenHandler, catchHandler) {
     const {token, cancel} = axios.CancelToken.source();
     options.cancelToken = token;
-    const request = axios(options).then(thenHandler).catch(catchHandler)
+    //
+    let request = axios(options)
+    if (thenHandler)
+        request = request.then(thenHandler)
+    if (catchHandler)
+        request = request.catch(catchHandler)
+    //
     request.cancel = cancel;
     return request;
 }
